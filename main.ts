70027b7051b14c79899581b5f210bc761488b4ee
Ingo Hachelbusch <ingo.hachelbusch@kernpunkt.de>
2024-12-11 14:59:14 +0100

    use Deno.serve

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
-export default {
-    fetch(request: Request): Response {
-        return new Response("Hello, world!");
+Deno.serve({
+    onListen: ({ port }) => {
+        console.log("Deno server listening on *:", port);
     },
-};
+}, async (req: Request, conn: Deno.ServeHandlerInfo) => {
+    // Get information about the incoming request
+    const text = await req.text()
+    const ip = conn.remoteAddr.hostname;
+    console.log(`${ip} just made an HTTP request with the text: ${text}`);
+
+    // Return a web standard Response object
+    return new Response("Hello, world!");
+});
