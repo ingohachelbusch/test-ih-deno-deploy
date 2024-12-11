2469d695b718f56960189525497fefc562b69c44
Ingo Hachelbusch <ingo.hachelbusch@kernpunkt.de>
2024-12-11 16:58:32 +0100

    add handling for closed merge request and missing jira ticket numbers

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ import type { MergeRequestEvent } from "./gitlabHooks"
 
 // TODO make configurable
 
-// TODO test this with deno
 function extractJiraTicketFromMRTitle(title: string): string | null {
     const regex = /.*:.*\(([a-zA-Z]+-[0-9]+)\)/g
     return Array.from(title.matchAll(regex), m => m[1])?.[0] ?? null
@@ -19,20 +18,28 @@ Deno.serve(async (req: Request) => {
             const webhookEvent = JSON.parse(text)
             if (webhookEvent?.event_type === "merge_request") {
                 const event = webhookEvent as MergeRequestEvent
-                if (!event.object_attributes?.draft) {
-                    console.log('Merge request is not in draft!')
+                if (!event.object_attributes?.draft && event.object_attributes?.state === 'opened') {
+                    console.log('Process Merge request!')
                     const ticketNumber = extractJiraTicketFromMRTitle(event.object_attributes.title)
-                    console.log('JIRA Ticket number:', ticketNumber)
+                    if (ticketNumber) {
+                        console.log('JIRA Ticket number:', ticketNumber)
+                    } else {
+                        console.warn(`The JIRA ticket number in ${event.object_attributes.url} is missing!`)
+                    }
+
                     /* TODOs:
                     * - check if a github MR exists (determined on what information?)
                     * -- if not create one
                     * -- determine how to get the deployment url
                     * --- if not possible get the MR ID and build the id itself
-                    * -- write Preview URL + amt-URLs into the JIRA (Ticket number extracted from title)
+                    * -- write Preview URL + amt-URLs into the JIRA
                     * -- Optional: Write Message in MS Teams
                     */
-                } else {
-                    console.log('Merge request is in draft!')
+                } else if (event.object_attributes?.draft) {
+                    console.log('Ignore merge request because it is in draft!')
+                } else if (event.object_attributes?.state === 'closed') {
+                    // TODO close github MR
+                    console.log('Merge request is closed!!')
                 }
             } else {
                 console.log(`Unsupported webhook event: ${webhookEvent?.event_type}`)
